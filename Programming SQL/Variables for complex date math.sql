c694b38ef5c7c89c5795ba733bdc3b0a8972d9b0
Elena Popova <elipopovadev@gmail.com>
2022-03-27 21:29:01 +0200

    Variables for complex date math

---
+++ Programming SQL/Variables for complex date math.sql
@@ -0,0 +1,25 @@
+--Variables for complex date math:
+
+DECLARE @Today DATE = CAST(GETDATE() AS DATE)
+
+SELECT @Today
+
+DECLARE @BOM DATE = DATEFROMPARTS(YEAR(@Today),MONTH(@Today),1)
+
+SELECT @BOM 
+
+DECLARE @PrevEOM DATE = DATEADD(DAY,-1,@BOM)
+
+SELECT @PrevEOM
+
+DECLARE @PrevBOM DATE = DATEADD(MONTH,-1,@BOM)
+
+SELECT @PrevBOM
+
+
+
+SELECT
+*
+FROM AdventureWorks2019.dbo.Calendar
+WHERE DateValue BETWEEN @PrevBOM AND @PrevEOM
+
